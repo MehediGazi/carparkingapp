5cdb8bc660dd12bf3ac28300e39592815d05ebd4
Navely <nprima173068@bscse.uiu.ac.bd>
2022-05-20 22:43:05 +0600

    log in page query changed

--- parkingownerlogin_process.php
+++ parkingownerlogin_process.php
@@ -13,36 +13,7 @@
 
             $query="SELECT email FROM parkingowner WHERE email='$email' and pass='$pass'";
             
-            try{
-                $returnval=$dbcon->query($query);
-                if($returnval->rowCount()==1){
-
-                    session_start();
-                    
-                    $_SESSION['email']=$email;
-                    ?>
-                        <script>
-                            window.location.assign('home.html');
-                        </script>
-                    <?php
-                }
-                else{
-
-                    ?>
-                        <script>
-                            window.location.assign('carownerlogin.html');
-                            window.alert("Wrong Login Information. Try Again");
-                        </script>
-                    <?php
-                }
-            }
-            catch(PDOException $ex){
-                ?>
-                    <script>
-                        window.location.assign('signin.php');
-                    </script>
-                <?php
-            }
+           
         }
         catch(PDOException $ex){
             ?>
