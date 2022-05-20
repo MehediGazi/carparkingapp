bbb3604c4814599776779ae8bf9091c310acf8f2
Rownok <rownok2001@gmail.com>
2022-05-20 22:02:06 +0600

    rownok

--- carownersignup_process.php
+++ carownersignup_process.php
@@ -12,45 +12,6 @@
 		$gender = $_POST["gender"];
 		$phone = $_POST["phone"];
 
-		try {
-
-			$dbcon = new PDO("mysql:host=localhost:3306;dbname=carparking;","root","");
-			$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-			$quary = "INSERT INTO carowner(firstname, lastname, gender, email, pass, phone, car) VALUES('$fn','$ln','$gender','$email','$pass','$phone','$car')";
-			echo $quary;
-
-			try {
-				
-				$dbcon->exec($quary);
-
-				session_start();
-                    
-				$_SESSION['email']=$email;
-
-
-				$_SESSION['fn'] = $fn;
-				$_SESSION['ln'] = $ln;
 	
-
-				?>
-					<script>window.location.assign('carhome.php')</script>
-				<?php
-			} catch (PDOExpection $ex) {
-				?>
-					<script>window.location.assign('signup.php')</script>
-				<?php
-			}
-			
-		} catch (PDOExpection $ex) {
-			?>
-					<script>window.location.assign('signup.php')</script>
-			<?php
-		}
-	}else{
-			?>
-				<script>window.location.assign('signup.php')</script>
-			<?php
-	}
 ?>
 
