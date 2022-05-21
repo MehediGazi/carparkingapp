c10606b08fd4c725aad00977b51eb64a9920d0cd
Navely <nprima173068@bscse.uiu.ac.bd>
2022-05-22 00:07:24 +0600

    parking home applied  with ajax modifed

--- parkinghome_ajax.php
+++ parkinghome_ajax.php
@@ -3,12 +3,7 @@
 $id = $_GET['id'];
 $conn = mysqli_connect("localhost", "root", "", "carparking");
 $result = mysqli_query($conn, "SELECT * FROM parkingowner WHERE id = '$id'");
- 
-$data = array();
-while ($row = mysqli_fetch_object($result))
-{
-    array_push($data, $row);
-}
+
  
 echo json_encode($data);
 exit();
