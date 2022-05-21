b94abb302379f11a7e08d413e071982ed783b189
MehediGazi <mehedi.gazi.me@gmail.com>
2022-05-22 00:55:26 +0600

    car sign process up fixed

--- carownersignup_process.php
+++ carownersignup_process.php
@@ -1,56 +1,56 @@
 <?php
 
 
-if( isset($_POST['fn']) && isset($_POST['ln']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['addr']) && isset($_POST['car']) && isset($_POST['gender']) && !empty($_POST["pass"]) ){
+	if( isset($_POST['fn']) && isset($_POST['ln']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['addr']) && isset($_POST['car']) && isset($_POST['gender']) && !empty($_POST["pass"]) ){
 
-	$fn = $_POST["fn"];
-	$ln = $_POST["ln"];
-	$email = $_POST["email"];
-	$pass = md5($_POST["pass"]);
-	$loc = $_POST["addr"];
-	$car = $_POST["car"];
-	$gender = $_POST["gender"];
-	$phone = $_POST["phone"];
-
-	try {
-
-		$dbcon = new PDO("mysql:host=localhost:3306;dbname=carparking;","root","");
-		$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$quary = "INSERT INTO carowner(firstname, lastname, gender, email, pass, phone, car) VALUES('$fn','$ln','$gender','$email','$pass','$phone','$car')";
-		echo $quary;
+		$fn = $_POST["fn"];
+		$ln = $_POST["ln"];
+		$email = $_POST["email"];
+		$pass = md5($_POST["pass"]);
+		$loc = $_POST["addr"];
+		$car = $_POST["car"];
+		$gender = $_POST["gender"];
+		$phone = $_POST["phone"];
 
 		try {
-			
-			$dbcon->exec($quary);
 
-			session_start();
+			$dbcon = new PDO("mysql:host=localhost:3306;dbname=carparking;","root","");
+			$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$quary = "INSERT INTO carowner(firstname, lastname, gender, email, pass, phone, car) VALUES('$fn','$ln','$gender','$email','$pass','$phone','$car')";
+			echo $quary;
+
+			try {
 				
-			$_SESSION['email']=$email;
+				$dbcon->exec($quary);
+
+				session_start();
+                    
+				$_SESSION['email']=$email;
 
 
-			$_SESSION['fn'] = $fn;
-			$_SESSION['ln'] = $ln;
+				$_SESSION['fn'] = $fn;
+				$_SESSION['ln'] = $ln;
+	
 
-
+				?>
+					<script>window.location.assign('carhome.php')</script>
+				<?php
+			} catch (PDOExpection $ex) {
+				?>
+					<script>window.location.assign('signup.php')</script>
+				<?php
+			}
+			
+		} catch (PDOExpection $ex) {
 			?>
-				<script>window.location.assign('carhome.php')</script>
+					<script>window.location.assign('signup.php')</script>
 			<?php
 		}
-		
-
-		
-		
-	} catch (PDOExpection $ex) {
-		?>
+	}else{
+			?>
 				<script>window.location.assign('signup.php')</script>
-		<?php
+			<?php
 	}
-	
-}else{
-		?>
-			<script>window.location.assign('signup.php')</script>
-		<?php
-}
 ?>
 
