b224434133aadf3803531f299804919dc2cfc268
MehediGazi <mehedi.gazi.me@gmail.com>
2022-05-22 00:56:14 +0600

    search parking space bug fixed

--- searchparkingspace_process.php
+++ searchparkingspace_process.php
@@ -49,7 +49,89 @@ $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   </head>
   <body>
         <!-- animations -->
-      
+        <header class="header">
+          <nav class=" fixed-top ">
+              <div class="container">
+                  <div class="wrapper d-flex align-items-center justify-content-between">
+                      <div class="logo d-flex align-items-center mb-0 mb-md-3">
+                          <img style="width:100px; height: 50px;margin-left: -10px;" src="img/carlogo.png" alt="">
+                          <span class="mt-2 text-uppercase">Garir Bari</span>
+                      </div>
+                      <input type="radio" name="slide" id="menu-btn">
+                      <input type="radio" name="slide" id="cancel-btn">
+                      <ul class="nav-links">
+                          <label for="cancel-btn" class="btn cancel-btn"> <i class="fas fa-times"></i>
+                                      </label>
+                          <li><a href="index.html" class="active">Home</a></li>
+                          <li><a href="about.html">About us</a></li>
+                       
+                          
+                          <li> <a class="login" href="help.html">Contact Us</a></li>
+                          
+                      </ul>
+                      <label for="menu-btn" class="btn menu-btn"> <i class="fas fa-bars"></i> </label>
+                  </div>
+              </div>
+          </nav>
+      </header>
+  
+  
+    <section class="blogs" id="blogs">
+      <p class=" container font-weight-bold fs-2">Search Result</p>
+      <div class="d-flex justify-content-around flex-wrap">
+
+        <!-- <div class="box p-3 m-2">
+          <div class="image">
+            <img src="img/car 1.png" class="w-100 h-100" alt="" />
+          </div>
+          <div class="content p-2 text-center">
+            <h3>Mohommodpur</h3>
+
+            <p class="">House no 20 mohommodpur dhaka</p>
+
+            <a style="margin-top: 40px" href=""> 12$</a>
+            <a href="" class="link-btn font-weight-bold">Book Now</a>
+          </div>
+        </div> -->
+        <?php
+          $query1="SELECT * FROM parkingspace WHERE area = '$area' ORDER BY road1 ASC";
+
+          $returnval1=$dbcon->query($query1);
+          $info1 = $returnval1->fetchAll();
+          $no = 1;
+          foreach($info1 as $row1){
+              $district = $row1['district'];
+              $area = $row1['area'];
+              $road1 = $row1['road1'];
+              $road2 = $row1['road2'];
+              $house = $row1['house'];
+              $spacenum = $row1['spacenum'];
+              $rent = $row1['rent'];
+              $img = $row1['img'];
+              if($img == ""){
+                $img = "img/parking/default.jpeg";
+              }
+              
+              ?>
+
+        <div class="box p-3 m-2">
+            <div class="image">
+              <img src="<?php echo $img;?>" class="w-100 h-100 img-thumbnail" alt="Image not found" />
+            </div>
+            <div class="content p-2 text-center">
+              <h3><?php echo $area;?></h3>
+  
+              <p class=""><?php echo "House: ",$house, ", Road: ", $road1, $road2, ", Area: ",$area, " District: ", $district;?></p>
+  
+              <a style="margin-top: 40px" href=""><?php echo $rent, ' Taka/Hour';?></a>
+              <a href="" class="link-btn font-weight-bold">Book Now</a>
+            </div>
+          </div>
+          <?php
+            $no = $no+1;
+            }
+          ?>
+
           <!-- <div class="box p-3 m-2">
             <div class="image">
               <img src="img/car 1.png" class="w-100 h-100" alt="" />
